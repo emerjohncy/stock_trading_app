84b786f8cd3705d543a5350f62e13926ce279eff
grailwilly <grailjwi11y@gmail.com>
2022-08-03 19:08:40 +0800

    Add user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_in_path(resources)
-    if current_user.admin?
-      redirect admin_path
-    else
-      redirect home_path
-    end
-  end
+  # def after_sign_in_path(resources)
+  #   if current_user.admin?
+  #     redirect admin_path
+  #   else
+  #     redirect home_path
+  #   end
+  # end
    
   protected
 
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class DashboardController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :except => :home
 
   def home
   end
