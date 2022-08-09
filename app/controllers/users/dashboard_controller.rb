7585fdc2b8bd9fbab04b635b52b772729274df5b
grailwilly <grailjwi11y@gmail.com>
2022-08-09 22:23:04 +0800

    Add conditional pending users

--- app/controllers/users/dashboard_controller.rb
+++ app/controllers/users/dashboard_controller.rb
@@ -1,4 +1,13 @@
 class Users::DashboardController < ApplicationController
   def index
+    @user = User.all 
+    @user.each do |user|
+      if user.status == "pending"
+        flash[:notice] = "Waiting for approval"
+        redirect_to root_path and return
+      else
+        redirect_to users_authenticated_root 
+      end
+    end
   end
 end
