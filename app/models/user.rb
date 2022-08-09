490f544e6eb2f6f7819ae707be6a4f885517d455
grailwilly <grailjwi11y@gmail.com>
2022-08-09 19:18:54 +0800

    Add enum user status

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable  
+
+  enum status: [:pending, :active]
+  after_initialize :set_default_status, :if => :new_record?
+
+  def set_default_status
+    self.role ||= :pending
+  end
 end
