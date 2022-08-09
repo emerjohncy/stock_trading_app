07e7e9ad001feb8854ee3afed8c4ac0c026d2e4b
grailwilly <grailjwi11y@gmail.com>
2022-08-09 19:21:03 +0800

    Set default status

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ApplicationRecord
   after_initialize :set_default_status, :if => :new_record?
 
   def set_default_status
-    self.role ||= :pending
+    self.status ||= :pending
   end
 end
