5aa2b475069e7be7957c9a5d581111b956d1e4d1
Emer John Niones <nionesemerjohn@gmail.com>
2022-08-11 19:48:56 +0800

    create user mailer view

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-    default from: 'notifications@stockuser.com'
+    default from: 'notifications@stockname.com'
 
     def welcome_email
         @user = params[:user]
