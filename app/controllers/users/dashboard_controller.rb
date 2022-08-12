305f227c96aab6a2f9ba3e7cc011d8bc0e149f7c
Emer John Niones <nionesemerjohn@gmail.com>
2022-08-12 21:59:38 +0800

    modify portfolio method in controller

--- app/controllers/users/dashboard_controller.rb
+++ app/controllers/users/dashboard_controller.rb
@@ -20,6 +20,30 @@ class Users::DashboardController < ApplicationController
   end
 
   def portfolio
+    @user_stocks = Hash.new
+    @stocks_owned = []
+
+    @buy_transactions = @user.transactions.where(action: "Buy")
+    @sell_transactions = 
+    @stocks_transacted_buy = @user.transactions.where(action: "Buy").map{ |transaction| transaction.stock }.uniq
+    # @stocks_owned = @stocks_transacted_buy.uniq
+
+    # Number of Units per Stock
+    for i in 0...@stocks_transacted_buy.length do
+      @user_stocks[@stocks_transacted_buy[i].name] = 0
+      @stock_transactions = @user.transactions.where(action: "Buy", stock_id: @stocks_transacted_buy[i].id)
+      @stock_transactions.each do |transaction|
+        @user_stocks[@stocks_transacted_buy[i].name] += transaction.units
+      end
+    end
+
+    @user_stocks.each do |stock, units|
+      if units != 0
+        @stocks_owned.push(stock)
+      end
+    end
+
+    
   end
 
   private
